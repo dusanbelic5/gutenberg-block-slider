d9643fcae4715d1402a07fbd57f981b888b84bd1
Dusan Belic <dusan.belic@everon.swiss>
2025-07-24 15:58:35 +0200

    small fix

--- src/slider/edit.js
+++ src/slider/edit.js
@@ -173,7 +173,6 @@ export default function Edit({ attributes, setAttributes }) {
                 </TabPanel>
 			</InspectorControls>
 
-
 			<div {...blockProps}>
 				<ServerSideRender
 					block="create-block/slider"
